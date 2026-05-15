9269d003e83b78a30ec3ec04492fba4e98a4621a
sayarshakya <sakyashire@hotmail.com>
2026-05-15 12:59:37 +0545

    Solution folder

---
+++ MyApp.API/Controllers/StudentController.cs
@@ -0,0 +1,42 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyApp.Application.Commands;
+using MyApp.Application.Queries;
+using MyApp.Domain.Entities;
+
+namespace MyApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController(IMediator mediator) : ControllerBase
+    {
+        [HttpGet("GetAllStudents")]
+        public async Task<IActionResult> GetAllStudents()
+        {
+            var result = await mediator.Send(new GetAllStudentsQuery());
+            return Ok(result);
+        }
+
+        [HttpPost("AddStudent")]
+        public async Task<IActionResult> AddStudent([FromBody]Student student)
+        {
+            var result = await mediator.Send(new AddStudentCommand(student));
+            return Ok(result);
+        }
+
+        [HttpDelete("DeleteStudent/{id}")]
+        public async Task<IActionResult> DeleteStudent([FromRoute]int id)
+        {
+            var result = await mediator.Send(new DeleteStudentCommand(id));
+            return Ok(result);
+        }
+
+        [HttpPut("UpdateStudent/{id}")]
+        public async Task<IActionResult> AddStudent([FromRoute] int id, [FromBody] Student student)
+        {
+            var result = await mediator.Send(new UpdateStudentCommand(id, student));
+            return Ok(result);
+        }
+    }
+}
---
+++ MyApp.API/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using MyApp.Application;
+using MyApp.Infrastructure;
+
+namespace MyApp.API
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApiDI(this IServiceCollection services)
+        {
+            services.AddInfrastructureDI().AddApplicationDI();
+            return services;
+        }
+    }   
+}
